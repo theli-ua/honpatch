a13cd8d3b5c6d6d1fe16dc65e0d96786978d5acd
Jonas Obrist <me@ojii.ch>
2011-12-18 20:32:38 +0100

    fixed typo

--- honpatch.py
+++ honpatch.py
@@ -326,7 +326,7 @@ def main():
         print('{0} == {1}, nothing to update'.format(curManifest.version,destver))
         exit(0)
 
-    print('Doing upgrade from source dir "{0}" to destination "{1}",\n\t version {2} => {3}'.format(options.hondir,destdir,curManifest.version,destver))
+    print('Upgrading from source dir "{0}" to destination "{1}",\n\t version {2} => {3}'.format(options.hondir,destdir,curManifest.version,destver))
 
     print('Fetching manifest.xml for {0}'.format(destver))
 
